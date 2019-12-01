e8a7d4b81039c12e44998f32d804004360666165
pedox <25@wamplo.id>
2019-12-01 11:25:43 +0700

    Updates

---
+++ precache-manifest.934a8d02abd1eebcce73739c0bb24d34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18356d384acf3e68248f7ad32916ff6",
+    "url": "/react-skeleton/index.html"
+  },
+  {
+    "revision": "de5bbd13ade6da69cab6",
+    "url": "/react-skeleton/static/css/main.4d799493.chunk.css"
+  },
+  {
+    "revision": "c6bacfbdd13d258972e7",
+    "url": "/react-skeleton/static/js/2.42238400.chunk.js"
+  },
+  {
+    "revision": "de5bbd13ade6da69cab6",
+    "url": "/react-skeleton/static/js/main.b535be74.chunk.js"
+  },
+  {
+    "revision": "5fb2e9ae6226e79274ba",
+    "url": "/react-skeleton/static/js/runtime-main.bb6cf598.js"
+  }
+]);
